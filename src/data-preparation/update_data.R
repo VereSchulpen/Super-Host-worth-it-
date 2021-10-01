5c7ebabcd21deaa5231c1aec44adeec783dcfe11
Albirizzu <albertomaria.rizzuti@gmail.com>
2021-10-01 14:58:21 +0200

    rm update data and add the code in clean_data

--- src/data-preparation/update_data.R
+++
@@ -1,7 +0,0 @@
-#######################
-#### UPDATE DATA ######
-#######################
-
-
-dir.create("./gen/data-preparation/input")
-file.copy("./data/dataset1/venice1.csv","./gen/data-preparation/input/venice1.csv")
