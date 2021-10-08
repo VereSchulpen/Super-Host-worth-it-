8d28534b4c39d30a80019c682d4595597b1a7b57
Davejongenelen <89848762+Davejongenelen@users.noreply.github.com>
2021-10-08 17:41:44 +0200

    Update analysis.R

--- src/analysis/analysis.R
+++ src/analysis/analysis.R
@@ -4,6 +4,10 @@ load("./gen/analysis/input/data_cleaned.RData")
 # Calculate the average price of a room in Venice
 mean(df_cleaned$price_numeric)
 
+# Create a subset of the mean prices of a superhost vs a non-superhost
+mean <- subset(df_cleaned, !is.na(price_numeric) & !is.na(host_is_superhost_binary)) %>% group_by(host_is_superhost_binary) %>% summarize(mean = mean(price_numeric)) 
+View(mean)
+
 #Model1
 m1 <- lm(price_num ~ host_is_superhost, df_cleaned)
 
