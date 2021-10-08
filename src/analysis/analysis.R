3580d07a878487e19bd568e9c04a9b6bf37d70ae
Davejongenelen <89848762+Davejongenelen@users.noreply.github.com>
2021-10-08 17:41:06 +0200

    Update analysis.R

--- src/analysis/analysis.R
+++ src/analysis/analysis.R
@@ -1,11 +1,8 @@
 #loaddata
 load("./gen/analysis/input/data_cleaned.RData")
 
-# Dependent variable transformation -> make the variable numeric
-df_cleaned$price_num <- as.numeric((gsub("[$,]","", df_cleaned$price)))
-# Independent variable transformation -> make the variable binary with 1 = superhost and 0 = non-superhost
-df_cleaned$host_is_superhost_TRUE <- ifelse(df_cleaned$host_is_superhost == 'TRUE', 1, 0)
-df_cleaned$host_is_superhost_TRUE[is.na(df_cleaned$host_is_superhost_TRUE)] <- 0 
+# Calculate the average price of a room in Venice
+mean(df_cleaned$price_numeric)
 
 #Model1
 m1 <- lm(price_num ~ host_is_superhost, df_cleaned)
