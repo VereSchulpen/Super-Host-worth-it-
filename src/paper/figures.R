ee909bb17d31a5dd9d7801566a449bc54b3b9fa3
Davejongenelen <89848762+Davejongenelen@users.noreply.github.com>
2021-10-08 15:50:24 +0200

    Update figures.R

--- src/paper/figures.R
+++ src/paper/figures.R
@@ -2,10 +2,14 @@
 #### CLEAN DATA ######
 ######################
 #Load library
-library()
+library(ggplot2)
 
 # Boxplot of the Dependent variable: price_numeric
 boxplot(df_cleaned$price_numeric)
 
 # Barplot of the Indepedent variable: host_is_super_binary
 ggplot(df_cleaned, aes(host_is_superhost_binary)) + geom_bar(aes(y = (..count..)/sum(..count..)*100)) + ylab("percentage")
+
+# combination of DV and IV in one boxplot
+ggplot(df_cleaned, aes(x=as.factor(host_is_superhost_binary), y=price_numeric)) + geom_boxplot()
+
