e3c9bd0519f69e3bb157d24907d314c149cc2188
Davejongenelen <89848762+Davejongenelen@users.noreply.github.com>
2021-10-08 17:43:29 +0200

    Update analysis.R

--- src/analysis/analysis.R
+++ src/analysis/analysis.R
@@ -4,6 +4,14 @@ load("./gen/analysis/input/data_cleaned.RData")
 # Calculate the average price of a room in Venice
 mean(df_cleaned$price_numeric)
 
+# Compare the price of a specific host with the average price of all hosts in Venice
+# So a we can see if the host is priced above the average or below the average
+if(df_cleaned$price_numeric[1] > mean(df_cleaned$price_numeric)){
+  print("This room is priced above the average price")
+} else {
+  print("This room is priced below the average price")
+}
+
 # Create a subset of the mean prices of a superhost vs a non-superhost
 mean <- subset(df_cleaned, !is.na(price_numeric) & !is.na(host_is_superhost_binary)) %>% group_by(host_is_superhost_binary) %>% summarize(mean = mean(price_numeric)) 
 View(mean)
