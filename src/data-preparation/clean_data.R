54386c3b9b8b4f8e149764609953fe67b883665e
Albirizzu <albertomaria.rizzuti@gmail.com>
2021-10-12 14:23:38 +0200

    dir.create

--- src/data-preparation/clean_data.R
+++ src/data-preparation/clean_data.R
@@ -6,6 +6,8 @@ library(tidyverse)
 library(gtsummary)
 
 # Create a copy in gen/input
+dir.create("./gen")
+dir.create("./gen/data-preparation")
 dir.create("./gen/data-preparation/input")
 file.copy("./data/dataset1/venice1.csv","./gen/data-preparation/input/venice1.csv")
 
--- src/paper/update_input.R
+++ src/paper/update_input.R
@@ -1,3 +1,4 @@
 # Copy output from data-preparation into input folder 
+dir.create("./gen/paper")
 dir.create("./gen/paper/input/")
 file.copy("./gen/data-preparation/output/data_cleaned.RData","./gen/paper/input/data_cleaned.RData")
