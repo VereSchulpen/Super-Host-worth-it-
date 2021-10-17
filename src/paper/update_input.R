919791e71752cad79b1bfcff0dd178d9ac362710
Albirizzu <albertomaria.rizzuti@gmail.com>
2021-10-17 17:52:25 +0200

    delete useless plots

--- src/paper/update_input.R
+++
@@ -1,8 +0,0 @@
-#####################
-### UPDATE INPUT ####
-#####################
-
-# Copy output from data-preparation into input folder 
-dir.create("./gen/paper")
-dir.create("./gen/paper/input/")
-file.copy("./gen/data-preparation/output/data_cleaned.RData","./gen/paper/input/data_cleaned.RData")
